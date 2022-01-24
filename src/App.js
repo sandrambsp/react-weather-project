59fe375b148e2e44abbbb12f26ccffb216e58899
sandrambsp <sandrambspereira@gmail.com>
2022-01-24 01:08:35 +0000

    added the github repo url

--- src/App.js
+++ src/App.js
@@ -3,5 +3,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Sandra Pereira an it is{" "}
+        <a
+          href="https://github.com/sandrambsp/react-weather-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>
+      </footer>
+    </div>
+  );
 }
