e53a8bcabe4ba529f4f536880e35ae672dc25bb8
sandrambsp <sandrambspereira@gmail.com>
2022-01-25 00:47:47 +0000

    added API integration

---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <a
+        href="https://github.com/sandrambsp/react-weather-project"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Open-source code
+      </a>{" "}
+      by Sandra Pereira
+    </div>
+  );
+}
--- src/Weather.js
+++ src/Weather.js
@@ -1,86 +1,120 @@
-import React from "react";
+import React, { useState } from "react";
+import Footer from "./Footer";
+import axios from "axios";
 
 import "./Weather.css";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <div className="card">
-        <div className="shadow-lg rounded">
-          <div className="card-body">
-            <div className="container-search">
-              <form className="city-form">
-                <input type="button" className="current-location" value="📍" />
-                <input
-                  type="search"
-                  className="city-input"
-                  placeholder="Search for a city"
-                />
-                <input type="submit" value="Search" />
-              </form>
-            </div>
-            <div>
-              <p>
-                Last updated: Monday 13:00 <span className="today"></span>
-              </p>
-            </div>
-            <div className="container-currently">
-              <div className="row align-items-center">
-                <div className="col-sm-4">
-                  <ul>
-                    <li className="city">
-                      <h2 className="current-city">Porto</h2>
-                    </li>
-                    <li>
-                      <p>
-                        <span className="description">Clear Sky</span>
-                      </p>
-                    </li>
-                    <li>
-                      <h3>
-                        <span className="temperature">8</span>
-                        <span className="units">ºC</span>
-                      </h3>
-                    </li>
-                  </ul>
-                </div>
-                <div className="col-sm-4">
-                  <img className="current-icon" src="" width="120px" alt="" />
-                </div>
-                <div className="col-sm-4">
-                  <ul className="col-right">
-                    <li className="wind">
-                      <img src="images/wind.svg" width="25px" alt="" />
-                    </li>
-                    <li className="wind">
-                      <span className="wind-speed">10</span> km/h
-                    </li>
-                  </ul>
-                  <ul className="col-right">
-                    <li className="humid">
-                      <img src="images/raindrop.svg" width="25px" alt="" />
-                    </li>
-                    <li className="humid">
-                      <span className="humidity">48</span>%
-                    </li>
-                  </ul>
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      date: "Monday 13:00",
+      temperature: Math.round(response.data.main.temp),
+      description: response.data.weather[0].description,
+      wind: Math.round(response.data.wind.speed),
+      humidity: response.data.main.humidity,
+      city: response.data.name,
+      icon: response.data.weather[0].icon,
+      id: response.data.weather[0].id,
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <div className="card">
+          <div className="shadow-lg rounded">
+            <div className="card-body">
+              <div className="container-search">
+                <form className="city-form">
+                  <input
+                    type="button"
+                    className="current-location"
+                    value="📍"
+                  />
+                  <input
+                    type="search"
+                    className="city-input"
+                    placeholder="Search for a city"
+                  />
+                  <input type="submit" value="Search" />
+                </form>
+              </div>
+              <div>
+                <p>
+                  Last updated:{" "}
+                  <span className="today">{weatherData.date}</span>
+                </p>
+              </div>
+              <div className="container-currently">
+                <div className="row align-items-center">
+                  <div className="col-sm-4">
+                    <ul>
+                      <li className="city">
+                        <h2 className="current-city">{weatherData.city}</h2>
+                      </li>
+                      <li>
+                        <p>
+                          <span className="description">
+                            {weatherData.description}
+                          </span>
+                        </p>
+                      </li>
+                      <li>
+                        <h3>
+                          <span className="temperature">
+                            {weatherData.temperature}
+                          </span>
+                          <span className="units">ºC</span>
+                        </h3>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="col-sm-4">
+                    <img
+                      className="current-icon"
+                      src="./images/{weatherData.icon}{weatherData.id}.svg"
+                      width="120px"
+                      alt=""
+                    />
+                  </div>
+                  <div className="col-sm-4">
+                    <ul className="col-right">
+                      <li className="wind">
+                        <img src="./images/wind.svg" width="25px" alt="" />
+                      </li>
+                      <li className="wind">
+                        <span className="wind-speed">{weatherData.wind}</span>{" "}
+                        km/h
+                      </li>
+                    </ul>
+                    <ul className="col-right">
+                      <li className="humid">
+                        <img src="./images/raindrop.svg" width="25px" alt="" />
+                      </li>
+                      <li className="humid">
+                        <span className="humidity">{weatherData.humidity}</span>
+                        %
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="container forecast"></div>
-            <div className="signature">
-              <a
-                href="https://github.com/sandrambsp/Weather-App"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Open-source code
-              </a>{" "}
-              by Sandra Pereira
+              <div className="container forecast"></div>
+              <Footer />
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "fc98cc0850552ccf4d2c0fb4439fee04";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
